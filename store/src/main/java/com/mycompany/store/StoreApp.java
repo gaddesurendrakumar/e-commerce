1ba54e7abbf73af2823071b05d5dbdddb8daad7b
sgadde <gaddesurendrakumar@gmail.com>
2020-10-21 11:24:39 +0530

    Added Docker support

--- store/src/main/java/com/mycompany/store/StoreApp.java
+++ store/src/main/java/com/mycompany/store/StoreApp.java
@@ -12,6 +12,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.core.env.Environment;
 
@@ -24,6 +25,7 @@ import java.util.Collection;
 @SpringBootApplication
 @EnableConfigurationProperties({LiquibaseProperties.class, ApplicationProperties.class})
 @EnableZuulProxy
+@EnableDiscoveryClient
 public class StoreApp {
 
     private static final Logger log = LoggerFactory.getLogger(StoreApp.class);
